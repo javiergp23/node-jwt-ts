9eaf3b070fb3a442deb8196a9467eeecc4bdd093
javiergp23 <javgon2306@gmail.com>
2025-04-15 21:44:22 -0300

    add docker

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import {register} from '../controller/authController';
+
 
 const router = express.Router();
 
-router.post('/register', () => {})
+router.post('/register', register)
 router.post('/login', () => {
 
 })
